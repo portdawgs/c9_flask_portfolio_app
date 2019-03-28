d1cc483b93c5fd98529f3241dd2e0ea102d6f824
portdawgs <48984287+portdawgs@users.noreply.github.com>
2019-03-28 01:45:13 -0700

    Update app.py

--- app.py
+++ app.py
@@ -15,7 +15,7 @@ def home_page():
 
 @app.route('/<name>')
 def profile(name):
-	new_name = str(name) + "likes to eat oranges."
+	new_name = str(name) + " likes to eat oranges"
 	return render_template('index.html', name=new_name)
 
 
